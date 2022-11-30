2365968c24b50878b0523f2868724cb3c5e07c82
JoseTheChamp <apep.knotek@gmail.com>
2022-11-30 15:51:53 +0100

    Support for landscape

--- app/src/main/java/com/company/bmta_sem/view/HeroDetailActivity.kt
+++ app/src/main/java/com/company/bmta_sem/view/HeroDetailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.company.bmta_sem.R
 import com.company.bmta_sem.databinding.ActivityHeroDetailBinding
 import com.company.bmta_sem.databinding.ActivityHeroesGalleryBinding
+import com.company.bmta_sem.model.GameProvider
 
 class HeroDetailActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHeroDetailBinding
@@ -14,7 +15,16 @@ class HeroDetailActivity : AppCompatActivity() {
         binding = ActivityHeroDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var hero = GameProvider.game?.currentHero!!
 
+        binding.txtHeroDetailName.text = hero.name
+        binding.txtDes.text = hero.description
 
+        binding.txtCha.text = hero.charisma.toString()
+        binding.txtDex.text = hero.dexterity.toString()
+        binding.txtCon.text = hero.constitution.toString()
+        binding.txtInt.text = hero.inteligence.toString()
+        binding.txtWis.text = hero.wisdom.toString()
+        binding.txtStr.text = hero.strength.toString()
     }
 }
